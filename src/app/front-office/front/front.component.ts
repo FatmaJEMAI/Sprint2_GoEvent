547fee2d1508c366bcbb8e6123ae39b72481c033
FatmaJEMAI <fatma.jemai@esprit.tn>
2022-11-26 22:14:55 +0100

    Modification FrontOffice

--- src/app/front-office/front/front.component.ts
+++ src/app/front-office/front/front.component.ts
@@ -1,20 +1,13 @@
 import { Component, OnInit,Renderer2 } from '@angular/core';
 import { ScriptServiceServiceService } from "../../script-service-service.service";
 const SCRIPT_PATH_LIST =[
-  "../../../assets/FrontOffice/js/audio.js",
-  "../../../assets/FrontOffice/js/audiojs.js",
+  "../../../assets/FrontOffice/js/jquery.min.js",
   "../../../assets/FrontOffice/js/bootstrap.min.js",
-  "../../../assets/FrontOffice/js/coundown-timer.js",
-  "../../../assets/FrontOffice/js/gmaps.js",
-  "../../../assets/FrontOffice/js/holder.js",
-  "../../../assets/FrontOffice/js/html5shiv.js",
-  "../../../assets/FrontOffice/js/jquery.countTo.js",
-  "../../../assets/FrontOffice/js/jquery.fitvids.js",
-  "../../../assets/FrontOffice/js/jquery.isotope.min.js",
-  "../../../assets/FrontOffice/js/jquery.js",
-  "../../../assets/FrontOffice/js/lightbox.min.js",
-  "../../../assets/FrontOffice/js/main.js",
-  "../../../assets/FrontOffice/js/masonry.min.js",
+  "../../../assets/FrontOffice/js/jquery.sticky.js",
+  "../../../assets/FrontOffice/js/jquery.magnific-popup.min.js",
+  "../../../assets/FrontOffice/js/magnific-popup-options.js",
+  "../../../assets/FrontOffice/js/click-scroll.js",
+  "../../../assets/FrontOffice/js/custom.js",
 
   ] 
 
